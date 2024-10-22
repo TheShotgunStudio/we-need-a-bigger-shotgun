227be311f3ba5d68e59b60f7c39e2b45e1f21959
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2024-10-22 12:06:20 +0200

    Refactors for more clear variables, Implemented Gravity Priority, Reordered some folders.

---
+++ Project/Assets/Scripts/Gravity/GravityManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityManager : MonoBehaviour
+{
+    [HideInInspector]
+    public List<StaticGravityField> GravityFields;
+
+    private Rigidbody _rigidBody;
+    private void Awake()
+    {
+        this._rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (GravityFields.Count <= 0) return;
+        int highestPriority = GravityFields.Max(field => field.Priority);
+        Debug.Log("highest Priority: "+ highestPriority);
+        IEnumerable<StaticGravityField> highestPriorityFields = GravityFields.Where(field => field.Priority == highestPriority);
+        foreach (StaticGravityField gravityField in highestPriorityFields)
+        {
+            Debug.Log(gravityField.name + " Priority: " + gravityField.Priority + "Gravity Force: " + gravityField.GravityForce);
+            _rigidBody.AddForce(gravityField.GravityForce);
+        }
+    }
+}
