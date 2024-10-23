17df58801f508f56329a4b36f3b51e5fc354c3ef
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2024-10-23 10:26:41 +0200

    Created Weapon script. Temporary name, Added to Player

---
+++ Project/Assets/Scripts/Weapons/Dev/WeaponHitDetection.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponHitDetection : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Image Crosshair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
