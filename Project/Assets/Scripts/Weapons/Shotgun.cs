3ae7e7c660003e540f2fab3d1136bac673897915
Dotsially <mateu9999@gmail.com>
2024-10-23 16:50:55 +0200

    Fixed wrong shotgun inheritance

--- Project/Assets/Scripts/Weapons/Shotgun.cs
+++ Project/Assets/Scripts/Weapons/Shotgun.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
 
-public class Shotgun : MonoBehaviour
+public class Shotgun : Weapon
 {
     public Transform MainCamera;   
     public Transform ExplosionPosition;
