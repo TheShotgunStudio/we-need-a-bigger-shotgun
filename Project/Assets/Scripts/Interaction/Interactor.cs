b4cd7bfa1bd2296f3dfcd5a82988372001d922ca
KevinGit9 <93190942+KevinGit9@users.noreply.github.com>
2024-10-23 17:04:20 +0200

    Adds an prompt that pops up if the player can interact with an object

--- Project/Assets/Scripts/Interaction/Interactor.cs
+++ Project/Assets/Scripts/Interaction/Interactor.cs
@@ -12,25 +12,55 @@ public class Interactor : MonoBehaviour
     private float _interactionPointRadius;
     [SerializeField]
     private LayerMask _interactableMask;
-
     private readonly Collider[] _colliders = new Collider[3];
     private int _collidersFound;
+    private GameObject _currentInteractableChild;
 
     private void Update()
     {
         _collidersFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
 
-        if (_collidersFound <= 0) return;
+        if (_collidersFound <= 0)
+        {
+            DeactivateCurrentChild();
+            return;
+        }
 
         if (_colliders[0].TryGetComponent<IInteractable>(out var interactable))
         {
             if (interactable.Used) return;
 
+            // Get the child GameObject
+            GameObject interactableChild = _colliders[0].transform.GetChild(0).gameObject;
+
+            if (interactableChild != _currentInteractableChild)
+            {
+                // Deactivate the currently active child if it exists
+                if (_currentInteractableChild != null)
+                {
+                    _currentInteractableChild.SetActive(false);
+                }
+
+                // Activate the new interactable child
+                interactableChild.SetActive(true);
+                _currentInteractableChild = interactableChild;
+            }
+
             if (Keyboard.current.eKey.wasPressedThisFrame)
             {
                 interactable.Interact(this);
+                DeactivateCurrentChild();
             }
         }
+
+    }
+
+    private void DeactivateCurrentChild()
+    {
+        if (_currentInteractableChild == null) return;
+
+        _currentInteractableChild.SetActive(false);
+        _currentInteractableChild = null;
     }
 
     private void OnDrawGizmos()
--- Project/Assets/Scripts/Interaction/UpgradeMachine.cs
+++ Project/Assets/Scripts/Interaction/UpgradeMachine.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UpgradeMachine : MonoBehaviour, IInteractable
 {
     public UpgradeDisplay UpgradeDisplay;
+    public TextMeshProUGUI PromptText;
     [SerializeField]
     private string _prompt;
     public string InteractionPrompt => _prompt;
     [SerializeField]
     private bool _used = false;
     public bool Used => _used;
+    private Renderer _renderer;
+    [SerializeField]
+    private Material _materialAfterUse;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+
+        PromptText.text = _prompt;
+    }
 
     public bool Interact(Interactor interactor)
     {
@@ -18,6 +30,11 @@ public class UpgradeMachine : MonoBehaviour, IInteractable
         UpgradeDisplay.Initialize();
 
         _used = true;
+
+        if (_renderer != null && _materialAfterUse != null)
+        {
+            _renderer.material = _materialAfterUse;
+        }
 
         return true;
     }
